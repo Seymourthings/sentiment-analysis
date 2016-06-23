bc27fac49367b3f7337dafa8c558c7b95c841598
Agustin Bautista <agustin.bautista@priceline.com>
2016-06-23 17:22:00 -0400

    adding sentiment analysis api

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,15 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
 
-	$scope.tagline = 'To the moon and back!';	
+	$scope.tagline = 'To the moon and back!';
+
+	// $http({
+	// 	method: 'GET',
+	// 	url: '/test-api'
+	// }).then(function successCallback(response){
+	// 	$scope.test = response;
+	// 	console.log(response);
+	// }, function errorCallback(error){
+	// 	console.log('error: '+ error);
+	// });
 
 });
