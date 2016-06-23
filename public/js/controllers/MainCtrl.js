869aab4567a88150aaf7b740724100f737ff693a
Austin Liu <austin.liu@priceline.com>
2016-06-23 16:08:38 -0400

    feat(adding basic front-end functionality)

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,4 +2,10 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	$scope.tagline = 'To the moon and back!';	
 
+	$scope.tempData = ["asdf", "temp", "data", "derp", "qwer", "desea"];
+	$scope.currentFocus = $scope.tempData[0];
+
+	$scope.switchFocus = function(newFocus){
+		$scope.currentFocus = newFocus;
+	}
 });
