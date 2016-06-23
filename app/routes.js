6cc88dd5343da11ebe21825638a25d9b7c9341e1
Desean Abraham <desean.abraham@gmail.com>
2016-06-23 14:15:51 -0400

    added twitter module, routes changed a little

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,19 @@
 // frontend routes =========================================================
 // route to handle all angular requests
 
+function twitterRest(){
+	var twitter = require('twitter');
+	var twitterKeys = new twitterAPI({
+		consumerKey: 'JA654spy6WFvkyt3nam7LqFCK',
+		consumerSecret: 'lUj8Du3qkbolTsbnbe4pNKZgZv730qqdZVzBJuQ87U5jRx8Ewu',
+		callback: 'http://127.0.0.1',
+	});
+	console.log('Everything is dandy so far');
+	
+}
+
 function serve(app){
+	twitterRest();
 	app.get('*', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
