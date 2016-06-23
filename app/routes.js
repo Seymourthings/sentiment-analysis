2bd1fdc6754bb8cd7c953f34ab5265afc1fad882
Desean Abraham <desean.abraham@gmail.com>
2016-06-23 13:41:21 -0400

    added nodemon to package, also re-arranged routes.js slightly

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,14 @@
-module.exports = function(app) {
+// server routes ===========================================================
+// handle things like api calls
+// authentication routes
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
+// frontend routes =========================================================
+// route to handle all angular requests
 
-	// frontend routes =========================================================
-	// route to handle all angular requests
+function serve(app){
 	app.get('*', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
+}
 
-};
+module.exports = serve;
