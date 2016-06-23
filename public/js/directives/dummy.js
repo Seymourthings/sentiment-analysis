0df7d7a1e07e48a1e42b45da3e11653d84f93b67
Austin Liu <austin.liu@priceline.com>
2016-06-23 12:28:25 -0400

    chore(): using different starter

--- public/js/directives/dummy.js
+++
@@ -1,7 +0,0 @@
-app.directive('dummy', function () {
-	return {
-		restrict: "E",
-		replace: false,
-		templateUrl: '/templates/directives/dummy.html'
-	}
-});
--- server.js
+++ server.js
@@ -1,51 +1,30 @@
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 
-/**
- * Module dependencies.
- */
-var logger = require('winston');
+// configuration ===========================================
+	
+// config files
+var db = require('./config/db');
 
-var pjson = require('./package.json');
-var env = process.env.NODE_ENV = pjson.env || 'development';
+var port = process.env.PORT || 8080; // set our port
+// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
-var express = require('express');
-var pathChecker = require('./server/path_checker.js');
-var app = module.exports = express();
+// get all data/stuff of the body (POST) parameters
+app.use(bodyParser.json()); // parse application/json 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-if (env === 'production') {
-    app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000/'));
-    app.use(require('compression')({
-        filter: function(req, res) {
-            return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
-        },
-        level: 9
-    }));
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
-    require('./server/sitemap_from_routes')(app, 'http://localhost:8080');  //simple sitemap generated from angular routes definition
-}
+// routes ==================================================
+require('./app/routes')(app); // pass our application into our routes
 
-app.use(require('static-favicon')());
-app.set('showStackError', true);
-
-app.use(express.static('./public/'));
-app.use(require('morgan')('dev'));
-
-app.use(require('body-parser')()); 						// pull information from html in POST
-app.use(require('method-override')()); 					// simulate DELETE and PUT
-
-var port = process.env.PORT || pjson.port;
-var server = app.listen(port, function () {
-
-    app.get('*', function(req, res){
-        var pathName = req._parsedUrl.pathname;
-
-        if(pathChecker(pathName)){
-            res.sendfile('./public/index.html');
-        } else {
-            res.status(404);
-            res.sendfile('./public/index.html');
-        }
-
-    });
-    logger.info("Express server listening on port %d in %s mode", this.address().port, env);
-});
-
+// start app ===============================================
+app.listen(port);	
+console.log('Magic happens on port ' + port); 			// shoutout to the user
+exports = module.exports = app; 						// expose app
