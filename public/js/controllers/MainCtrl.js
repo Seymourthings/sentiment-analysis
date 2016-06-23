499716ad39bff2a5373704915778f0835a562571
Austin Liu <austin.liu@priceline.com>
2016-06-23 16:20:13 -0400

    chore(): slight styling

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'To the moon and back!';	
+	// $scope.tagline = 'To the moon and back!';	
 
 	$scope.tempData = ["asdf", "temp", "data", "derp", "qwer", "desea"];
 	$scope.currentFocus = $scope.tempData[0];
