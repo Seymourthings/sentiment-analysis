192b13e74958295a0e155155a74bfd4f729684fa
Austin Liu <austin.liu@priceline.com>
2016-06-24 10:07:04 -0400

    hooking together

--- app/routes.js
+++ app/routes.js
@@ -16,19 +16,58 @@ function twitterRest(){
 	return twitterKeys;
 }
 
+function alchemyRest(){
+	var AlchemyAPI = require('alchemy-api');
+	return alchemy = new AlchemyAPI('e1fd7bc4f36090d76a3efb0b0328081e29ab1ec7');
+}
+
+function alchemyProcess(statuses){
+	var alchemy = alchemyRest();
+
+	return new Promise(function(resolve, reject) {
+	  // do a thing, possibly async, then…
+		var processed = [];
+
+	  	for(status in statuses){
+			var params = {text: status.text};
+
+			alchemy.emotions("TEXT", params, function(err, response) {
+			  if (err) throw err;
+
+			  // See http://www.alchemyapi.com/api/html-api-1 for format of returned object
+			  var emotions = response.docEmotions;
+
+			  // Do something with data
+			  console.log(emotions);
+			  processed.push(status);
+			});
+		}
+
+		if (processed.length > 0) {
+		  resolve(processed);
+		}
+		else {
+		  reject(Error("It broke"));
+		}
+	});
+}
 
 function serve(app, res, req){
 	
 	var url = 'search/tweets';
 	var params = {
-		q: 'priceline'
+		q: 'priceline',
+		count: 2
 	}
 	var twitter = twitterRest();
 
 	app.get('/twitter', function(req, res) {
 		 
 		twitter.get(url, params, function(error, tweets, response){
-			res.send(tweets);	
+			alchemyProcess(tweets.statuses).then(function(data){
+				console.log('YAAAAY');
+				res.send(data);		
+			});
 		});
 	});
 }
