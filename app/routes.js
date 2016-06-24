7bb74f14ad9fcd21452f88683a06bd21c71dcc3c
Desean Abraham <desean.abraham@gmail.com>
2016-06-24 10:46:51 -0400

    seperated twitter objects by query

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,21 @@
 // frontend routes =========================================================
 // route to handle all angular requests}
 
+const priceline = '@priceline';
+const expedia = '@expedia';
+const orbitz = '@orbitz';
+const hipmunk = '@thehipmunk';
+const tripAdvisor = '@TripAdvisor';
+
+
+var pricelineTweet;
+var expediaTweet;
+var orbitzTweet;
+var hipmunkTweet;
+var tripAdvisorTweet;
+
+var twitter = twitterRest();
+
 function twitterRest(){
 	var twitter = require('twitter');
 	var twitterKeys = new twitter({
@@ -17,20 +32,73 @@ function twitterRest(){
 }
 
 
-function serve(app, res, req){
-	
+function expediaTweets(){
 	var url = 'search/tweets';
 	var params = {
-		q: 'priceline'
+		q: expedia,
+		count: '2'
 	}
-	var twitter = twitterRest();
-
-	app.get('/twitter', function(req, res) {
-		 
-		twitter.get(url, params, function(error, tweets, response){
-			res.send(tweets);	
-		});
+	
+	twitter.get(url, params, function(error, tweets, response){
+		expediaTweet = pullText(tweets, params);
+		console.log(expediaTweet);
 	});
 }
 
-module.exports = serve;
+function orbitzTweets(){
+	var url = 'search/tweets';
+	var params = {
+		q: orbitz,
+		count: '2'
+	}
+	twitter.get(url, params, function(error, tweets, response){
+		orbitzTweet = pullText(tweets, params);
+		console.log(orbitzTweet);	
+	});
+}
+
+function hipmunkTweets(){
+	var url = 'search/tweets';
+	var params = {
+		q: hipmunk,
+		count: '2'
+	}
+	twitter.get(url, params, function(error, tweets, response){
+		hipmunkTweet = pullText(tweets, params);
+		console.log(hipmunkTweet);	
+	});
+}
+
+function tripAdvisorTweets(){
+	var url = 'search/tweets';
+	var params = {
+		q: orbitz,
+		count: '2'
+	}
+	twitter.get(url, params, function(error, tweets, response){
+		tripAdvisorTweet = pullText(tweets, params);
+		console.log(tripAdvisorTweets);	
+	});
+}
+
+
+
+function pullText(tweets, params){	
+	var tweetText = [];
+	for (var status in tweets.statuses){
+		tweetText.push(tweets.statuses[status].text);
+	}
+	return tweetText;
+}
+
+function serveTweets(app){
+	app.get('/twitter', function(req, res){
+		orbitzTweets();
+		expediaTweets();
+		tripAdvisorTweets();
+		hipmunkTweets();
+		res.send(' ');
+	});
+}
+
+module.exports = serveTweets;
